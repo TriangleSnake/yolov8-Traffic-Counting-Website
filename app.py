50e29cd56409aa27a051d8dbcce627974d7bd263
TriangleSnake <trianglesnake2002@gmail.com>
2024-01-13 01:20:26 +0800

    Finish draw function

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ def check_step(token):# 0.影片還沒上傳 1.畫線階段 2.數車階段 3.上
         return 2
     elif os.path.exists('./uploads/'+token+'/snapshot.png'):
         return 1
+    else:
+        return 0
 
 def is_vaild_token(token):
     with open('token.txt', 'r') as f:
@@ -49,9 +51,12 @@ def index():
 
 @app.get('/upload')
 def upload_page():
-    if is_vaild_token(request.cookies.get('token')):
-        return render_template('upload.html')
-    return make_response('Forbidden', 403)
+    token = request.cookies.get('token')
+    if not is_vaild_token(token):
+        return make_response('Forbidden', 403)
+    if check_step(token) > 0:
+        return redirect(url_for('draw'))
+    return render_template('upload.html')
 
 @app.post('/upload')
 def upload():
@@ -63,16 +68,19 @@ def upload():
 
     file = request.files['file']
     if file and allowed_filetype(file.filename):
+        import os
+        if not os.path.exists('./uploads/'+token):
+            os.mkdir('./uploads/'+token)
         filepath = './uploads/'+request.cookies.get('token')+'/' + "video."+file.filename.rsplit('.', 1)[1]
         file.save(filepath)
         import cv2
-        video = cv2.videoCapture(filepath)
+        video = cv2.VideoCapture(filepath)
         video.set(cv2.CAP_PROP_POS_MSEC, 5000)
         ret, frame = video.read()
         cv2.imwrite('./uploads/'+token+'/snapshot.png', frame)
         return make_response('OK', 200)
 
-@app.route('draw', methods=['GET', 'POST'])
+@app.route('/draw', methods=['GET', 'POST'])
 def draw():
     token = request.cookies.get('token')
     if not is_vaild_token(token):
@@ -87,16 +95,23 @@ def draw():
         return render_template('draw.html')
     elif request.method == 'POST':
         points = request.form['points']
+        response = make_response(redirect(url_for('result')))
+        response.set_cookie('area', points)
         '''
         開畫分析
         '''
-        return redirect(url_for('result', points=points))
+        return response
 
-@app.route('result')
+@app.route('/result')
 def result():
     token = request.cookies.get('token')
     if not is_vaild_token(token):
         return make_response('Forbidden', 403)
+    points = request.cookies.get('area')
+    with open('./uploads/'+token+'/area.txt', 'w') as f:
+        f.write(points)
+
+    return make_response('Success! We\'re processing your video...', 200)
 
 
 
